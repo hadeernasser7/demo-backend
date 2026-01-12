68032011462e3459288250f425648972ad6ff1dd
Hadeer Nasser <elfazary200@gmail.com>
2026-01-13 01:36:16 +0200

    first commitdata

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-app.get('/', (req, res) => res.send('Backend is Running!'));
-app.listen(5000, () => console.log('Server on port 5000'));
+
+// سطر الاتصال بالداتابيز بالباسورد الجديد
+const mongoURI = "mongodb+srv://hadeer:12345@cluster0.4drd9vh.mongodb.net/GraduationDB?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('✅ تم الاتصال بمونجو بنجاح!'))
+  .catch(err => console.error('❌ فشل الاتصال:', err));
+
+app.get('/', (req, res) => {
+  res.send('<h1>الباك إند متصل بالداتابيز وشغال زي الفل!</h1>');
+});
+
+app.listen(5000, () => console.log('Server is running on port 5000'));
